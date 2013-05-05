b6cee8d51890d2e5b1d16fa09d25947d44d4d463
amewo <amewo22370@gmail.com>
2013-05-05 21:47:47 +0400

    ???

--- neuro_net_genetic_training.cpp
+++ neuro_net_genetic_training.cpp
@@ -1,2 +1,98 @@
 #include "neuro_net_genetic_training.h"
 
+//-----------------------------------------------------------------------------
+time_stamp_distributor::time_stamp_distributor() noexcept
+    : m_free_link_time_stamp(1)
+    , m_free_node_time_stamp(1)
+{
+}
+//-----------------------------------------------------------------------------
+void time_stamp_distributor::reset() noexcept
+{
+    m_node_time_stampes.clear();
+    m_link_time_stampes.clear();
+
+    m_free_link_time_stamp = 1;
+    m_free_node_time_stamp = 1;
+}
+//-----------------------------------------------------------------------------
+uint32_t time_stamp_distributor::get_time_stamp_for_node(uint32_t link_time_stamp,
+                                                         uint32_t& link_in_time_stamp,
+                                                         uint32_t& link_out_time_stamp)
+{
+    auto it = m_node_time_stampes.find(link_time_stamp);
+
+    //todo:???
+
+    uint32_t node_time_stamp;
+
+    if( it == m_node_time_stampes.end() )
+    {
+        node_time_stamp = m_free_node_time_stamp;
+        ++m_free_node_time_stamp;
+
+        m_node_time_stampes[link_time_stamp] = node_time_stamp;
+    }
+    else
+    {
+        node_time_stamp = it->second;
+    }
+
+    return node_time_stamp;
+}
+//-----------------------------------------------------------------------------
+uint32_t time_stamp_distributor::get_time_stamp_for_link(uint32_t node_in_time_stamp, uint32_t node_out_time_stamp)
+{
+    auto it = m_link_time_stampes.find(node_in_time_stamp);
+
+    uint32_t link_time_stamp;
+
+    if( it == m_link_time_stampes.end() )
+    {
+        link_time_stamp = m_free_link_time_stamp;
+        ++m_free_link_time_stamp;
+
+        m_link_time_stampes[node_in_time_stamp][node_out_time_stamp] = link_time_stamp;
+    }
+    else
+    {
+        std::map<uint32_t, uint32_t>& map = it->second;
+
+        auto it2 = map.find(node_out_time_stamp);
+
+        if( it2 == map.end() )
+        {
+            link_time_stamp = m_free_link_time_stamp;
+            ++m_free_link_time_stamp;
+
+            map[node_out_time_stamp] = link_time_stamp;
+        }
+        else
+        {
+            link_time_stamp = it2->second;
+        }
+    }
+
+    return link_time_stamp;
+}
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+population::population(uint32_t size, uint32_t in_signal_size, uint32_t out_signal_size) throw(std::runtime_error)
+    : m_training_patterns(in_signal_size, out_signal_size)
+{
+}
+//-----------------------------------------------------------------------------
+bool population::set_training_patterns(patterns &ptrns) noexcept
+{
+    if( m_training_patterns.get_in_size() != ptrns.get_in_size() ||
+        m_training_patterns.get_out_size() != ptrns.get_out_size()
+      )
+    {
+        return false;
+    }
+
+    m_training_patterns = ptrns;
+
+    return true;
+}
+//-----------------------------------------------------------------------------
--- neuro_net_genetic_training.h
+++ neuro_net_genetic_training.h
@@ -3,20 +3,25 @@
 
 #include <stdint.h>
 #include <vector>
+#include <stdexcept>
+#include <map>
 
+#include "patterns.h"
+
+//-----------------------------------------------------------------------------
 enum class node_activation_func_type : uint8_t
 {
     linear_activation_func,
     hyperbolic_activation_func
 };
-
+//-----------------------------------------------------------------------------
 enum class node_type : uint8_t
 {
     input_node,
     hidden_node,
     output_node
 };
-
+//-----------------------------------------------------------------------------
 struct node
 {
     node_activation_func_type activation_func;
@@ -28,7 +33,7 @@ struct node
     float sum;
     float signal;
 };
-
+//-----------------------------------------------------------------------------
 struct link
 {
     uint32_t time_stamp;
@@ -40,24 +45,55 @@ struct link
 
     bool enabled;
 };
-
+//-----------------------------------------------------------------------------
 struct individual
 {
     std::vector<node> nodes;
     std::vector<link> links;
-};
 
+    // Очередь, в соответствии с которой следует вычислять сигналы узлов.
+    std::vector<uint32_t> calc_queue;
+
+    // Если была добавлена новая связь или нейрон, то следует построить очередь расчета заново.
+    bool changed;
+};
+//-----------------------------------------------------------------------------
 class time_stamp_distributor
 {
 public:
     time_stamp_distributor() noexcept;
-    virtual ~time_stamp_distributor() noexcept;
 
-    void reset_full() noexcept;
-    void reset_to_new_iteration() noexcept;
+    void reset() noexcept;
 
-    uint32_t get_time_stamp_for_node(uint32_t link_time_stamp, uint32_t& link_from_time_stamp, uint32_t& link_to_time_stamp);
-    uint32_t get_time_stamp_for_link(uint32_t node_from_time_stamp, uint32_t node_to_time_stamp);
+    uint32_t get_time_stamp_for_node(uint32_t link_time_stamp, uint32_t& link_in_time_stamp, uint32_t& link_out_time_stamp);
+    uint32_t get_time_stamp_for_link(uint32_t node_in_time_stamp, uint32_t node_out_time_stamp);
+
+protected:
+    struct node_
+    {
+
+    };
+
+    std::map<uint32_t, uint32_t> m_node_time_stampes;
+    std::map<uint32_t, std::map<uint32_t, uint32_t>> m_link_time_stampes;
+
+    uint32_t m_free_link_time_stamp;
+    uint32_t m_free_node_time_stamp;
 };
+//-----------------------------------------------------------------------------
+class population
+{
+public:
+    population(uint32_t size, uint32_t in_signal_size, uint32_t out_signal_size) throw(std::runtime_error);
+
+    bool set_training_patterns(patterns &ptrns) noexcept;
+
+protected:
+    patterns                m_training_patterns;
+    time_stamp_distributor  m_time_stamp_distributor;
+
+    std::vector<individual> m_individuals;
+};
+//-----------------------------------------------------------------------------
 
 #endif // #define neuro_net_genetic_training_h
