f44aedf0dc9c5daf99aa6f6008967558df0644b9
amewo <amewo22370@gmail.com>
2013-05-12 23:35:40 +0400

    ???

--- neuro_net_test_app.cpp
+++ neuro_net_test_app.cpp
@@ -9,12 +9,92 @@
 #include <neuro_net_genetic_training.h>
 #include <bars.h>
 
+#include <boost/math/special_functions.hpp>
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
+
 #include <boost/lexical_cast.hpp>
 
 #include <QApplication>
 #include <qcp.h>
 
-using namespace std;
+boost::numeric::ublas::vector<float> decompose(boost::numeric::ublas::vector<float> sig)
+{
+    if( sig.size() % 2 == 1 )
+    {
+        throw 1;
+    }
+
+    boost::numeric::ublas::matrix<float> p(sig.size(), sig.size(), 0.0f);
+
+    for(int i = 0; i < sig.size() / 2; ++i)
+    {
+        p(i*2,i*2) = 1.0f / sqrt(2.0f);
+        p(i*2,i*2+1) = 1.0f / sqrt(2.0f);
+        p(i*2+1,i*2) = - 1.0f / sqrt(2.0f);
+        p(i*2+1,i*2+1) = 1.0f / sqrt(2.0f);
+    }
+
+    boost::numeric::ublas::vector<float> res = boost::numeric::ublas::prod(sig, p);
+
+    return res;
+}
+
+boost::numeric::ublas::vector<float> compose(boost::numeric::ublas::vector<float> coefs)
+{
+    if( coefs.size() % 2 == 1 )
+    {
+        throw 2;
+    }
+
+    boost::numeric::ublas::matrix<float> p(coefs.size(), coefs.size(), 0.0f);
+
+    for(int i = 0; i < coefs.size() / 2; ++i)
+    {
+        p(i*2,i*2) = 1.0f / sqrt(2.0f);
+        p(i*2,i*2+1) = 1.0f / sqrt(2.0f);
+        p(i*2+1,i*2) = - 1.0f / sqrt(2.0f);
+        p(i*2+1,i*2+1) = 1.0f / sqrt(2.0f);
+    }
+
+    p = trans(p);
+
+    boost::numeric::ublas::vector<float> res = boost::numeric::ublas::prod(coefs, p);
+
+    return res;
+}
+
+boost::numeric::ublas::vector<float> filter(uint32_t lvl, boost::numeric::ublas::vector<float> sig)
+{
+    if(lvl = 0)
+    {
+        return sig;
+    }
+
+    boost::numeric::ublas::vector<float> dec = decompose(sig);
+    boost::numeric::ublas::vector<float> sig2(dec.size()/2);
+
+    for( size_t i = 0; i < dec.size() / 2; ++i )
+    {
+        sig2(i) = dec(i*2+1);
+
+        if( dec(i*2) < 0.0002f )
+        {
+            dec(i*2) = 0.0f;
+        }
+    }
+
+    sig2 = filter(lvl - 1, sig2);
+
+    for( size_t i = 0; i < dec.size() / 2; ++i )
+    {
+        dec(i*2+1) = sig2(i);
+    }
+
+    return compose(dec);
+}
 
 int main(int argc, char *argv[])
 {
@@ -85,79 +165,107 @@ int main(int argc, char *argv[])
     cin.get();
     */
 
-    /*
     bars b;
     b.load_from_csv("/home/amewo/Documents/neural networks/test.csv");
-
-    cout << "bars count: " << b.get_size() << endl;
-
-    cin.get();
-    */
 
     /*
     population pop(320, 2, 1);
     pop.make_test();
     */
 
-    uint32_t n = 10000;
-
+    /*
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<uint32_t> dis(0, n - 1);
+    std::uniform_int_distribution<int32_t> dis(-1, 1);
+    std::uniform_real_distribution<double> dis2(-0.3, 0.3);
+    */
 
-    vector<uint32_t> v(n, 0);
-    QVector<double> x, y;
+    boost::numeric::ublas::vector<float> sig1(b.get_size() / 20), sig2(b.get_size() / 20);
 
-    for(size_t i = 0; i < 10000000; ++i)
+    for( size_t i = 0; i < b.get_size() / 20; ++i )
     {
-        v[dis(gen)] += 1;
+        sig1(i) = (b[i].high);
+        sig2(i) = (b[i].high);
     }
 
-    for( size_t i = 0; i < n; ++i )
+    sig1 = filter(1, sig1);
+
+//    boost::numeric::ublas::vector<float> dec1 = decompose(sig1);
+
+    QVector<double> x, y1, y2;
+/*
+    boost::numeric::ublas::vector<float> sig12(dec1.size() / 2);
+
+    for( size_t i = 0; i < sig12.size(); ++i )
+    {
+        sig12(i) = dec1(i*2+1);
+    }
+
+    boost::numeric::ublas::vector<float> dec12 = decompose(sig12);
+
+    for( size_t i = 0; i < dec12.size() / 2; ++i )
+    {
+        if( dec12(i*2) <= 0.0003f )
+        {
+            dec12(i*2) = 0.0f;
+        }
+    }
+
+    sig12 = compose(dec12);
+
+    for( size_t i = 0; i < sig12.size(); ++i )
+    {
+        dec1(i*2+1) = sig12(i);
+    }
+
+    for( size_t i = 0; i < dec1.size() / 2; ++i )
+    {
+        if( std::abs(dec1(i*2)) <= 0.0003f )
+        {
+            dec1(i*2) = 0.0f;
+        }
+    }
+
+    sig1 = compose(dec1);
+*/
+    for( size_t i = 0; i < b.get_size() / 20; ++i )
     {
         x.push_back(i);
-        y.push_back(v[i]);
+        y1.push_back(sig1(i));
+        y2.push_back(sig2(i));
     }
 
     QApplication a(argc, argv);
 
     QCustomPlot *customPlot = new QCustomPlot();
 
-    // For simplicity we'll just setup all data and plotting options here
-    // add two new graphs and set their look:
     customPlot->addGraph();
-    customPlot->graph(0)->setPen(QPen(Qt::blue)); // line color blue for first graph
-    customPlot->graph(0)->setBrush(QBrush(QColor(0, 0, 255, 20))); // first graph will be filled with translucent blue
-//    customPlot->addGraph();
-//    customPlot->graph(1)->setPen(QPen(Qt::red)); // line color red for second graph
-    // generate some points of data (y0 for first, y1 for second graph):
-//    QVector<double> x(250), y0(250), y1(250);
-//    for (int i=0; i<250; ++i)
-//    {
-//      x[i] = i;
-//      y0[i] = exp(-i/150.0)*cos(i/10.0); // exponentially decaying cosine
-//      y1[i] = exp(-i/150.0);             // exponential envelope
-//    }
-    // configure right and top axis to show ticks but no labels (could've also just called customPlot->setupFullAxesBox):
+    customPlot->addGraph();
+
+    customPlot->graph(0)->setPen(QPen(Qt::black)); // line color blue for first graph
+    customPlot->graph(1)->setPen(QPen(Qt::red)); // line color blue for first graph
+
+    customPlot->xAxis->setLabel("bar num");
+    customPlot->yAxis->setLabel("price");
+
     customPlot->xAxis2->setVisible(true);
-    customPlot->xAxis2->setTickLabels(false);
+    customPlot->xAxis2->setTickLabels(true);
     customPlot->yAxis2->setVisible(true);
-    customPlot->yAxis2->setTickLabels(false);
-    // make left and bottom axes always transfer their ranges to right and top axes:
+    customPlot->yAxis2->setTickLabels(true);
+
     QObject::connect(customPlot->xAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->xAxis2, SLOT(setRange(QCPRange)));
     QObject::connect(customPlot->yAxis, SIGNAL(rangeChanged(QCPRange)), customPlot->yAxis2, SLOT(setRange(QCPRange)));
-    // pass data points to graphs:
-    customPlot->graph(0)->setData(x, y);
-//    customPlot->graph(1)->setData(x, y1);
-    // let the ranges scale themselves so graph 0 fits perfectly in the visible area:
-    customPlot->graph(0)->rescaleAxes();
-    // same thing for graph 1, but only enlarge ranges (in case graph 1 is smaller than graph 0):
-//    customPlot->graph(1)->rescaleAxes(true);
-    // Note: we could have also just called customPlot->rescaleAxes(); instead
-    // make range moveable by mouse interaction (click and drag):
+
+    customPlot->graph(0)->setData(x, y1);
+    customPlot->graph(1)->setData(x, y2);
+
+    customPlot->rescaleAxes();
+
     customPlot->setRangeDrag(Qt::Horizontal | Qt::Vertical);
     customPlot->setRangeZoom(Qt::Horizontal | Qt::Vertical);
+
     customPlot->setInteraction(QCustomPlot::iSelectPlottables); // allow selection of graphs via mouse click
+
     customPlot->replot();
 
     customPlot->show();
